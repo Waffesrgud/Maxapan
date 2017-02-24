13ec95219a12c35bd99fafb1b17755d8a3c4ec6d
Waffesrgud <gcor8853@740s-pl-132.local>
2017-02-24 09:56:45 -0700

    for sure do this

--- src/space/model/SolarSystem.java
+++ src/space/model/SolarSystem.java
@@ -5,7 +5,11 @@ import space.model.*;
 
 public class SolarSystem {
 //This array will only be 1D, due to solar system's lack of solid-ish positioning so it'll just be closest to the sun to longest away
-Planet[] planets;
-public SolarSystem(int size, int
+	
+
+public SolarSystem(int size, String starType)
+{
+	
+}
 
 }
